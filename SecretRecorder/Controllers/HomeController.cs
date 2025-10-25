22f7dc38e7837b2c6a1b63490ef13c38c079b840
Collin Makwala <collinmakwala@gmail.com>
2025-10-25 19:28:33 +0200

    delete secrete recorder

--- SecretRecorder/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace SecretRecorder.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index() => View();
-    }
-}
